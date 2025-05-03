8217b535f1d372a1ae7daa6d6e70ad844d105d85
gallardo747 <132841308+gallardo747@users.noreply.github.com>
2025-05-03 00:54:30 -0300

    Commit 03-05

--- 1-DML/JOBS.sql
+++ 1-DML/JOBS.sql
@@ -3,3 +3,12 @@ select * from  [LOG].[AUDIT_JOBS];
 insert into [LOG].[AUDIT_JOBS]
 values
 ('TMP_Shippers',getdate(),SYSTEM_USER,1);
+insert into [LOG].[AUDIT_JOBS]
+values
+('TMP_Regions',getdate(),SYSTEM_USER,1);
+insert into [LOG].[AUDIT_JOBS]
+values
+('TMP_Territories',getdate(),SYSTEM_USER,1);
+insert into [LOG].[AUDIT_JOBS]
+values
+('TMP_Categories',getdate(),SYSTEM_USER,1);
