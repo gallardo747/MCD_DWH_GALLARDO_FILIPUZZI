4f22ef4a8e537d0ea9b39e83b8029801853bf888
gallardo747 <132841308+gallardo747@users.noreply.github.com>
2025-05-06 23:43:22 -0300

    Version 2025-05-06

--- 1-DML/JOBS.sql
+++ 1-DML/JOBS.sql
@@ -1,5 +1,3 @@
-select * from  [LOG].[AUDIT_JOBS];
-
 insert into [LOG].[AUDIT_JOBS]
 values
 ('TMP_Shippers',getdate(),SYSTEM_USER,1);
@@ -36,3 +34,75 @@ values
 insert into [LOG].[AUDIT_JOBS]
 values
 ('TMP_Countries',getdate(),SYSTEM_USER,1);
+insert into [LOG].[AUDIT_JOBS]
+values
+('STG_Shippers',getdate(),SYSTEM_USER,1);
+insert into [LOG].[AUDIT_JOBS]
+values
+('STG_Regions',getdate(),SYSTEM_USER,1);
+insert into [LOG].[AUDIT_JOBS]
+values
+('STG_Territories',getdate(),SYSTEM_USER,1);
+insert into [LOG].[AUDIT_JOBS]
+values
+('STG_Categories',getdate(),SYSTEM_USER,1);
+insert into [LOG].[AUDIT_JOBS]
+values
+('STG_Customers',getdate(),SYSTEM_USER,1);
+insert into [LOG].[AUDIT_JOBS]
+values
+('STG_Employee_Territories',getdate(),SYSTEM_USER,1);
+insert into [LOG].[AUDIT_JOBS]
+values
+('STG_Employee',getdate(),SYSTEM_USER,1);
+insert into [LOG].[AUDIT_JOBS]
+values
+('STG_Order_Details',getdate(),SYSTEM_USER,1);
+insert into [LOG].[AUDIT_JOBS]
+values
+('STG_Orders',getdate(),SYSTEM_USER,1);
+insert into [LOG].[AUDIT_JOBS]
+values
+('STG_Products',getdate(),SYSTEM_USER,1);
+insert into [LOG].[AUDIT_JOBS]
+values
+('STG_Suppliers',getdate(),SYSTEM_USER,1);
+insert into [LOG].[AUDIT_JOBS]
+values
+('STG_Countries',getdate(),SYSTEM_USER,1);
+insert into [LOG].[AUDIT_JOBS]
+values
+('DWH_Shippers',getdate(),SYSTEM_USER,1);
+insert into [LOG].[AUDIT_JOBS]
+values
+('DWH_Regions',getdate(),SYSTEM_USER,1);
+insert into [LOG].[AUDIT_JOBS]
+values
+('DWH_Territories',getdate(),SYSTEM_USER,1);
+insert into [LOG].[AUDIT_JOBS]
+values
+('DWH_Categories',getdate(),SYSTEM_USER,1);
+insert into [LOG].[AUDIT_JOBS]
+values
+('DWH_Customers',getdate(),SYSTEM_USER,1);
+insert into [LOG].[AUDIT_JOBS]
+values
+('DWH_Employee_Territories',getdate(),SYSTEM_USER,1);
+insert into [LOG].[AUDIT_JOBS]
+values
+('DWH_Employee',getdate(),SYSTEM_USER,1);
+insert into [LOG].[AUDIT_JOBS]
+values
+('DWH_Order_Details',getdate(),SYSTEM_USER,1);
+insert into [LOG].[AUDIT_JOBS]
+values
+('DWH_Orders',getdate(),SYSTEM_USER,1);
+insert into [LOG].[AUDIT_JOBS]
+values
+('DWH_Products',getdate(),SYSTEM_USER,1);
+insert into [LOG].[AUDIT_JOBS]
+values
+('DWH_Suppliers',getdate(),SYSTEM_USER,1);
+insert into [LOG].[AUDIT_JOBS]
+values
+('DWH_Countries',getdate(),SYSTEM_USER,1);
